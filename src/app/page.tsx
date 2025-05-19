3b609298828e81011a04147d3e2ab3190dbbd142
sourav mishra <souravvmishra@gmail.com>
2025-05-19 10:34:56 +0530

    removed dots

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,21 +11,12 @@ import ServicesSection from "@/components/sections/home/about";
 export default function HomePage() {
   return (
     <>
-      <div className="w-full h-px bg-gradient-to-r from-transparent via-border to-transparent"></div>
       <Hero />
-      <div className="w-full h-px bg-gradient-to-r from-transparent via-border to-transparent"></div>
       <CustomerSegmentation/>
-      {/* Subtle Divider Example */}
-
-      <div className="w-full h-px bg-gradient-to-r from-transparent via-border to-transparent"></div>
       <ServicesSection/>
-      <div className="w-full h-px bg-gradient-to-r from-transparent via-border to-transparent"></div>
       <Features />
-      <div className="w-full h-px bg-gradient-to-r from-transparent via-border to-transparent"></div>
       <AboutUsSection />
-      <div className="w-full h-px bg-gradient-to-r from-transparent via-border to-transparent"></div>
       <Testimonials />
-      <div className="w-full h-px bg-gradient-to-r from-transparent via-border to-transparent"></div>
       <CtaSection />
       
     </>
